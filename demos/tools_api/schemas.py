b50bf10a88e2792398d42744f3b0cce06d81e364
Kathryn Bello <kbello@sopelos2023.localdomain.stsci.edu>
2024-05-01 16:26:24 -0400

    new updates

--- demos/tools_api/schemas.py
+++ demos/tools_api/schemas.py
@@ -1,4 +1,5 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
+import re
 
 
 class BaseCar(BaseModel):
@@ -8,6 +9,19 @@ class BaseCar(BaseModel):
     color: str
     price: float
 
+    @field_validator("year")
+    @classmethod
+    def validate_year(cls, value: int) -> int:
+        if value <= 1886:
+            raise ValueError("year cannot be earlier than or equal to 1886- the year the car was invented")
+        return value
+
+    @field_validator("price")
+    @classmethod
+    def validate_price(cls, value: float) -> float:
+        if value <= 0.00:
+            raise ValueError("price cannot be less or equal to zero")
+        return value
 
 class CarCreate(BaseCar):
     pass
@@ -26,6 +40,20 @@ class BaseColor(BaseModel):
     name: str
     hex_code: str
 
+    @field_validator("name", "hex_code")
+    @classmethod
+    def validate_required(cls, value: str) -> str:
+        if not isinstance(value, str) or len(value) == 0:
+            raise ValueError("is required")
+        return value
+
+    @field_validator("hex_code")
+    @classmethod
+    def validate_hex_code(cls, value: str) -> str:
+        if not re.match(r"^#(?:[0-9a-fA-F]{3}){1,2}$", value):
+            raise ValueError("not a valid CSS hexcode")
+        return value.lower()
+
 
 class ColorCreate(BaseColor):
     pass
@@ -39,12 +67,15 @@ class Color(BaseColor):
         # from_orm = True
         from_attributes = True
 
+
 class BaseMessage(BaseModel):
     recipient_email: str
     message: str
 
+
 class MessageCreate(BaseMessage):
     pass
+
 
 class Message(BaseMessage):
     id: int
@@ -53,6 +84,7 @@ class Message(BaseMessage):
     class Config:
         # from_orm = True
         from_attributes = True
+
 
 class NotifyResponse(BaseModel):
     status: str
